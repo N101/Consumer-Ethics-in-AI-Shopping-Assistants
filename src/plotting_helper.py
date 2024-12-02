7b82b31af8f12579c1bd3bfbb943363cd97162d1
Noah <noah.kiefer@icloud.com>
2024-12-02 11:23:40 +0100

    capsize automatic sizing

--- src/plotting_helper.py
+++ src/plotting_helper.py
@@ -4,21 +4,25 @@ import seaborn as sns
 
 
 YLIM = (0, 5.9)
-SUFFIX = ""
 
 
 def make_graphs(
     df: pd.DataFrame, slices: list, labels: list, errors: pd.DataFrame, suffix: str
 ) -> list[plt.Figure]:
-    SUFFIX = suffix
     images = []
+    capsizes = {"downloading": 7, "passive": 3}
+    default_capsize = 4
+
+    def get_capsize(key):
+        return capsizes.get(key, default_capsize)
+    
     for sl, lbl in zip(slices, labels):
         fig, ax = plt.subplots()
         df.iloc[sl].plot(
             kind="bar",
             ylim=YLIM,
             yerr=errors,
-            capsize=3,
+            capsize=get_capsize(lbl),
             ecolor="darkred",
             color=["#2ca02c", "#4682b4", "#5a9bd4"],
             ax=ax,
@@ -27,16 +31,16 @@ def make_graphs(
             ylabel="Avg Score",
             figsize=(10, 5),
             rot=0,
-        ).legend([f"{SUFFIX}", "Students", "Non-students"])
+        ).legend([f"{suffix}", "Students", "Non-students"])
         fig.tight_layout()
         images.append(fig)
     return images
 
 
-def make_heatmap(df: pd.DataFrame) -> plt.Figure:
+def make_heatmap(df: pd.DataFrame, suffix: str) -> plt.Figure:
     fig, ax = plt.subplots()
     pivot_table = df.pivot_table(values="Response", index="Iteration", columns="#")
     sns.heatmap(pivot_table, cmap="viridis", cbar_kws={"label": "Responses"}, ax=ax)
-    ax.set_title(f"Heatmap {SUFFIX}")
+    ax.set_title(f"Heatmap {suffix}")
     fig.tight_layout()
     return fig
